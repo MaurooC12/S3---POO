10ecbe8a01941ddcf9ef72125a62236a0776f6ff
MaurooC12 <andrescpda12@gmail.com>
2025-09-02 15:49:06 -0500

    Organización del GitHub
     Changes to be committed:
    	modified:   Encuesta/encuesta.py
    
     Changes not staged for commit:
    	deleted:    Encuesta/encuesta.py
    	deleted:    Exercise in class/script.py
    
     Untracked files:
    	Lessons/
    	Surveys/

--- Encuesta/encuesta.py
+++ Encuesta/encuesta.py
@@ -1,7 +1,6 @@
+
 for i in range(7):
     nombrePersona = input("Por favor ingrese su nombre: ")
     carreraPersona = input("Por favor ingrese en qué carrera está: ")
     ideaPersona = input("Por favor ingrese la idea de su proyecto: ")
-    
-    
-    
+    print(f"Hola {nombrePersona} de la carrera {carreraPersona}, la idea de su proyecto es {ideaPersona}")
